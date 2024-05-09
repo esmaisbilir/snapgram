2707d6c0a6b841602f8e568f2da953d9f44d985b
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-05-09 13:09:03 +0300

    create post

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,6 +1,7 @@
 import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
-import { INewUser } from '@/types'
+import { QUERY_KEYS } from './queryKeys'
+import { createUserAccount, signInAccount, signOutAccount, createPost } from '../appwrite/api'
+import { INewUser, INewPost } from '@/types'
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -17,17 +18,20 @@ export const useSignInAccount = () => {
     })
 }
 
-/*export const useSignInAccount = () => {
-    return useMutation({
-        mutationFn: (user: { 
-            email: string; 
-            password: string 
-        }) => signInAccount(user),
-    })
-} */
-
 export const useSignOutAccount = () => {
     return useMutation({
         mutationFn: signOutAccount
     })
 }
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (post: INewPost) => createPost(post),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+      },
+    });
+  };
