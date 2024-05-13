d9097d7288d6af58d63b9ca565c9c408799ce6a9
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-05-13 23:24:08 +0300

    post details

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,7 +1,47 @@
+import Loader from "@/components/shared/Loader"
+import { useGetPostById } from "@/lib/react-query/queriesAndMutations"
+import { useParams, Link } from "react-router-dom"
+import { multiFormatDateString } from "@/lib/utils"
+
 const PostDetails = () => {
+  const { id } = useParams()
+  const { data: post, isPending } = useGetPostById(id || '')
+
   return (
-    <div>
-        PostDetails      
+    <div className="post_details-container">
+      {isPending ? <Loader /> : (
+        <div className="post_details-card">
+          <img 
+          src={post?.imageUrl} 
+          alt="post"
+          className="post_details-img" 
+          />
+            <div className="post_details-info">
+              <Link to={`/profile/${post?.creator.$id}`}>
+                <img 
+                src={post?.creator?.imageUrl || '/assets/icons/profile-placeholder.svg'} 
+                alt="creator"
+                className="rounded-full w-12 lg:h-12"
+                />
+              </Link>
+
+                <div className="flex flex-col">
+                  <p className="base-medium lg:body-bold text-light-1">
+                    {post?.creator.name}
+                  </p>
+                  <div className="flex-center gap-2 text-light-3">
+                    <p className="subtle-semibold lg:small-regular">
+                      {multiFormatDateString(post?.$createdAt)}
+                    </p>
+                    -
+                    <p className="subtle-semibold lg:small-regular">
+                      {post?.location}
+                    </p>
+                    </div>
+              </div>
+            </div>
+        </div>
+      )}
     </div>
   )
 }
