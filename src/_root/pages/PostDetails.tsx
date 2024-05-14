cb67512f935b8c7b407ef2d325fa49d5a24bf017
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-05-14 23:46:45 +0300

    post details info

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -2,10 +2,12 @@ import Loader from "@/components/shared/Loader"
 import { useGetPostById } from "@/lib/react-query/queriesAndMutations"
 import { useParams, Link } from "react-router-dom"
 import { multiFormatDateString } from "@/lib/utils"
+import { useUserContext } from "@/context/AuthContext"
 
 const PostDetails = () => {
   const { id } = useParams()
   const { data: post, isPending } = useGetPostById(id || '')
+  const { user } = useUserContext()
 
   return (
     <div className="post_details-container">
@@ -16,28 +18,41 @@ const PostDetails = () => {
           alt="post"
           className="post_details-img" 
           />
-            <div className="post_details-info">
-              <Link to={`/profile/${post?.creator.$id}`}>
-                <img 
-                src={post?.creator?.imageUrl || '/assets/icons/profile-placeholder.svg'} 
-                alt="creator"
-                className="rounded-full w-12 lg:h-12"
-                />
-              </Link>
 
-                <div className="flex flex-col">
+          <div className="post_details-info">
+            <div className="flex-between w-full">
+              <Link
+              to={`/profile/${post?.creator.$id}`}
+              className="flex items-center gap-3">
+                <img
+                src={
+                post?.creator.imageUrl ||
+                "/assets/icons/profile-placeholder.svg"
+                }
+                alt="creator"
+                className="w-8 h-8 lg:w-12 lg:h-12 rounded-full"
+                />
+                <div className="flex gap-1 flex-col">
                   <p className="base-medium lg:body-bold text-light-1">
                     {post?.creator.name}
                   </p>
                   <div className="flex-center gap-2 text-light-3">
-                    <p className="subtle-semibold lg:small-regular">
+                    <p className="subtle-semibold lg:small-regular ">
                       {multiFormatDateString(post?.$createdAt)}
                     </p>
-                    -
+                    •
                     <p className="subtle-semibold lg:small-regular">
                       {post?.location}
                     </p>
                     </div>
+                  </div>
+                </Link>
+
+                <div className="flex-center gap-4">
+                  <Link to={`/update-post/${post?.$id}`} className={`${user.id !== post?.creator.$id && 'hidden'}`}>
+                    <img src="/assets/icons/edit.svg" alt="edit" width={24} height={24} />
+                  </Link>
+                </div>
               </div>
             </div>
         </div>
