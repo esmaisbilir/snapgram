fcec02518813aace6aa4d4840c64fcf682c2ced0
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-05-17 16:19:36 +0300

    SearchResults

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -62,8 +62,8 @@ const Explore = () => {
       <div className="flex flex-wrap gap-9 w-full max-w-5xl">
         {shouldShowSearchResults ? (
           <SearchResults 
-            results={searchedPosts}
-            isLoading={isSearchFetching}
+            isSearchFetching={isSearchFetching}
+            searchedPosts={searchedPosts}
           />
         ) : shouldShowPosts ? (
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
