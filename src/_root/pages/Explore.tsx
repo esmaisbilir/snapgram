39a16881a66bc1f5013b974a312e20facfd28ffb
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-05-17 16:56:17 +0300

    explore page

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,17 +1,24 @@
+import { useState, useEffect } from "react"
 import { Input } from "@/components/ui/input"
-import { useState } from "react"
 import SearchResults from "@/components/shared/SearchResults"
 import GridPostList from "@/components/shared/GridPostList"
 import { useGetPosts, useSearchPosts } from "@/lib/react-query/queriesAndMutations"
 import useDebounce from "@/hooks/useDebounce"
 import Loader from "@/components/shared/Loader"
+import { useInView } from "react-intersection-observer";
+
 
 const Explore = () => {
+  const { ref, inView } = useInView()
   const { data: posts, fetchNextPage, hasNextPage } = useGetPosts()
 
   const [searchValue, setSearchValue] = useState('')
   const debouncedValue = useDebounce(searchValue, 500)
   const { data: searchedPosts, isFetching: isSearchFetching } = useSearchPosts(debouncedValue)  
+
+  useEffect(() => {
+    if(inView && !searchValue) fetchNextPage()
+  }, [inView, searchValue])
 
   if(!posts) {
     return (
@@ -73,6 +80,12 @@ const Explore = () => {
           ))
         )}
       </div>
+
+      {hasNextPage && !searchValue && (
+        <div ref={ref} className="mt-10">
+          <Loader />
+        </div>
+      )}
     </div>
   )
 }
